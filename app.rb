d554cea159d79ae4382ab577d7c1cda92c5c47fc
Ryan MacGillivray <rmacg89@gmail.com>
2018-06-05 21:30:45 +0100

    Initial index and show pages

--- app.rb
+++ app.rb
@@ -9,5 +9,39 @@ class App < Sinatra::Base
     get '/health' do
       json message: 'The Application is running'
     end
+
+    get '/rsvps' do
+      json [
+        {
+          name: 'Foo Bar',
+          dietary: '',
+          access_key: 'abc123'
+        },
+        {
+          name: 'Baz Qux and Bar Qux',
+          dietary: 'Gluten free',
+          access_key: 'xyz456'
+        }
+      ]
+    end
+
+    get '/rsvps/:access_key' do
+      json_response =
+        if params[:access_key] == 'abc123'
+          {
+            name: 'Foo Bar',
+            dietary: '',
+            access_key: 'abc123'
+          }
+        else
+          {
+            name: 'Baz Qux and Bar Qux',
+            dietary: 'Gluten free',
+            access_key: 'xyz456'
+          }
+        end
+
+      json(json_response)
+    end
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe App do
-  describe 'visiting /api/health' do
+  context 'visiting /api/health' do
     before { get '/api/health' }
 
     it 'returns a 200' do
@@ -8,6 +8,76 @@ RSpec.describe App do
 
     it 'returns "Hello" in the body' do
       expect(last_response.body).to include('Application is running')
+    end
+  end
+
+  describe 'RSVP routes' do
+    context 'visiting /api/rsvps' do
+      before { get '/api/rsvps' }
+
+      let(:rsvp_json) {
+        [
+          {
+            name: 'Foo Bar',
+            dietary: '',
+            access_key: 'abc123'
+          },
+          {
+            name: 'Baz Qux and Bar Qux',
+            dietary: 'Gluten free',
+            access_key: 'xyz456'
+          }
+        ].to_json
+      }
+
+      it 'returns a 200' do
+        expect(last_response).to be_ok
+      end
+
+      it 'returns a list of RSVPs' do
+        expect(last_response.body).to eq(rsvp_json)
+      end
+    end
+
+    describe 'access keys' do
+      context 'visiting /api/rsvps/abc123' do
+        before { get '/api/rsvps/abc123' }
+
+        let(:rsvp) {
+          {
+            name: 'Foo Bar',
+            dietary: '',
+            access_key: 'abc123'
+          }.to_json
+        }
+
+        it 'returns a 200' do
+          expect(last_response).to be_ok
+        end
+
+        it 'responds with the corresponding RSVP' do
+          expect(last_response.body).to eq(rsvp)
+        end
+      end
+
+      context 'visiting /api/rsvps/xyz456' do
+        before { get '/api/rsvps/xyz456' }
+        let(:rsvp) {
+          {
+            name: 'Baz Qux and Bar Qux',
+            dietary: 'Gluten free',
+            access_key: 'xyz456'
+          }.to_json
+        }
+
+        it 'returns a 200' do
+          expect(last_response).to be_ok
+        end
+
+        it 'responds with the RSVP' do
+          expect(last_response.body).to eq(rsvp) 
+        end
+      end
     end
   end
 end
